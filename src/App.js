969cbe424fe8fc7a8b326258532fc05ffd41c6ee
semenov-ol <56276169+semenov-ol@users.noreply.github.com>
2020-05-22 09:51:59 +0300

    Revert "Add Details Screen"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-
-import Main from './components/Main';
-import TenderCard from './components/TenderCard';
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Main} />
-
-      <Route path="/card" component={TenderCard} />
+      <span>Hello</span>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { ThemeProvider } from 'ustudio-ui/theme';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
